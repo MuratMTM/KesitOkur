db8d53d0d87b9f545a967cf4f9720f5001c7a24f
MuratMTM <murat.postmen@gmail.com>
2024-03-17 04:35:38 +0300

    kesitokur login page is created.

---
+++ KesitOkur/KesitOkur/KesitOkurApp.swift
@@ -0,0 +1,17 @@
+//
+//  KesitOkurApp.swift
+//  KesitOkur
+//
+//  Created by Murat Işık on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct KesitOkurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            IntroView()
+        }
+    }
+}
