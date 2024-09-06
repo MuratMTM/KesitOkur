d090213e5ef6262ff41d689245efcb5e6a33dabf
MuratMTM <murat.postmen@gmail.com>
2024-09-06 04:25:46 +0300

    kesitokur first step of networking model was coded

--- KesitOkur/KesitOkur/BookModel.swift
+++ KesitOkur/KesitOkur/BookModel.swift
@@ -16,6 +16,31 @@ struct Book: Identifiable, Decodable {
     let edition: String
     let pages: String
     let description: String
+    
+    
+}
 
-  
+func getBooks() async throws -> Book {
+    //continue later...
+    let endpoint: String = ""
+    guard let url = URL(string: endpoint) else {throw BookError.invalidURl}
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+        throw BookError.invalidResponse
+    }
+    do {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(Book.self, from: data)
+    } catch  {
+        throw BookError.invalidData
+    }
+}
+
+enum BookError: Error {
+    case invalidURl
+    case invalidResponse
+    case invalidData
 }
